8ef90e07770dcdb924ff53f74f12b6c9fcfd79c3
muhammadnuman1305 <muhammadnuman@falconit.com.pk>
2023-09-14 02:05:52 +0500

    New corrections

--- src/pages/loader/loader.jsx
+++ src/pages/loader/loader.jsx
@@ -19,45 +19,45 @@ const Loader = ({ setShowLoader }) => {
   }, [setShowLoader]);
 
   // Animation variants for the SVG icons
-  const iconVariant1 = {
-    hidden: {
-      pathLength: 0,
-      fill: "rgb(19, 26, 34)",
-    },
-    visible: {
-      pathLength: 5,
-      fill: "rgb(243, 243, 243)",
-      stroke: "rgb(243, 243, 243)",
-      strokeWidth: 3,
-      scale: 0,
-      transition: {
-        default: { duration: 3, ease: "easeIn" },
-        fill: { duration: 3, ease: [1, 0, 0.8, 1] },
-        stroke: { duration: 3, ease: [1, 0, 0.8, 1] },
-        scale: { duration: 3, ease: [1, 1, 0.5, 0] },
-      },
-    },
-  };
+  // const iconVariant1 = {
+  //   hidden: {
+  //     pathLength: 0,
+  //     fill: "rgb(19, 26, 34)",
+  //   },
+  //   visible: {
+  //     pathLength: 5,
+  //     fill: "rgb(243, 243, 243)",
+  //     stroke: "rgb(243, 243, 243)",
+  //     strokeWidth: 3,
+  //     scale: 0,
+  //     transition: {
+  //       default: { duration: 3, ease: "easeIn" },
+  //       fill: { duration: 3, ease: [1, 0, 0.8, 1] },
+  //       stroke: { duration: 3, ease: [1, 0, 0.8, 1] },
+  //       scale: { duration: 3, ease: [1, 1, 0.5, 0] },
+  //     },
+  //   },
+  // };
 
-  const iconVariant2 = {
-    hidden: {
-      pathLength: 0,
-      fill: "rgb(19, 26, 34)",
-    },
-    visible: {
-      pathLength: 5,
-      fill: "rgb(72, 163, 198)",
-      stroke: "rgb(72, 163, 198)",
-      scale: 0,
-      strokeWidth: 3,
-      transition: {
-        default: { duration: 3, ease: "easeIn" },
-        fill: { duration: 3, ease: [1, 0, 0.8, 1] },
-        stroke: { duration: 3, ease: [1, 0, 0.8, 1] },
-        scale: { duration: 3, ease: [1, 1, 0.5, 0] },
-      },
-    },
-  };
+  // const iconVariant2 = {
+  //   hidden: {
+  //     pathLength: 0,
+  //     fill: "rgb(19, 26, 34)",
+  //   },
+  //   visible: {
+  //     pathLength: 5,
+  //     fill: "rgb(72, 163, 198)",
+  //     stroke: "rgb(72, 163, 198)",
+  //     scale: 0,
+  //     strokeWidth: 3,
+  //     transition: {
+  //       default: { duration: 3, ease: "easeIn" },
+  //       fill: { duration: 3, ease: [1, 0, 0.8, 1] },
+  //       stroke: { duration: 3, ease: [1, 0, 0.8, 1] },
+  //       scale: { duration: 3, ease: [1, 1, 0.5, 0] },
+  //     },
+  //   },
+  // };
 
   return (
     <div
